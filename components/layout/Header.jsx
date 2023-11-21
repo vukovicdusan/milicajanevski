133762556d010a423f7b5a90f81c4f889c2b2c6d
Picvajzmen <picvajzmen@gmail.com>
2023-11-21 10:59:23 +0100

    Added scrollTo for kontakt link from the another page

--- components/layout/Header.jsx
+++ components/layout/Header.jsx
@@ -1,35 +1,48 @@
-import React from "react"
-import Wrapper from "./Wrapper"
-import * as css from "../../styles/Header.module.css"
-import Link from "next/link"
+import React, { useEffect, useState } from "react";
+import Wrapper from "./Wrapper";
+import * as css from "../../styles/Header.module.css";
+import Link from "next/link";
+import { useRouter } from "next/router";
 
 const Header = () => {
-	return (
-		<div>
-			<header className={css.header}>
-				<Wrapper>
-					<div className={css.headerWrap}>
-						<Link className={css.headerLogo} href={"/"}>
-							Milica Janevski
-						</Link>
-						<nav className={css.desktopNav}>
-							<ul className={css.headerWrap}>
-								<li>
-									<Link href={"/"}>Početna</Link>
-								</li>
-								<li>
-									<Link href={"./gallery"}>Galerija</Link>
-								</li>
-								<li>
-									<a href="#contact">Kontakt</a>
-								</li>
-							</ul>
-						</nav>
-					</div>
-				</Wrapper>
-			</header>
-		</div>
-	)
-}
+  const [mounted, setMounted] = useState(false);
+  let router = useRouter();
+  useEffect(() => {
+    let targetId = router.asPath.replace(/.*\#/, "");
+    const elem = document.getElementById(targetId);
+    setMounted(true);
+    window.scrollTo({
+      top: elem?.getBoundingClientRect().top,
+      behavior: "smooth",
+    });
+  }, [mounted]);
+  return (
+    <div>
+      <header className={css.header}>
+        <Wrapper>
+          <div className={css.headerWrap}>
+            <Link className={css.headerLogo} href={"/"}>
+              Milica Janevski
+            </Link>
+            <nav className={css.desktopNav}>
+              <ul className={css.headerWrap}>
+                <li>
+                  <Link href={"/"}>Početna</Link>
+                </li>
+                <li>
+                  <Link href={"/gallery"}>Galerija</Link>
+                </li>
+                <li>
+                  {/* <ScrollLink href="/#contact">Kontakt</ScrollLink> */}
+                  <a href="/#contact">Kontakt</a>
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </Wrapper>
+      </header>
+    </div>
+  );
+};
 
-export default Header
+export default Header;
