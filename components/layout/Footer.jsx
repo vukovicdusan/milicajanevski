7b1b14b508624daab026a53992c34583c5d2466a
Picvajzmen <picvajzmen@gmail.com>
2024-05-10 11:07:33 +0200

    Added credits link to footer

--- components/layout/Footer.jsx
+++ components/layout/Footer.jsx
@@ -1,20 +1,27 @@
-import React from "react"
-import Wrapper from "./Wrapper"
-import * as styles from "../../styles/Footer.module.css"
+import React from "react";
+import Wrapper from "./Wrapper";
+import * as styles from "../../styles/Footer.module.css";
 
 const Footer = () => {
-	let year = new Date().getFullYear()
-	return (
-		<footer className={styles.footer}>
-			<Wrapper>
-				<p className={styles.footerCenter}>
-					{" "}
-					Copyright © {year} Milica Janevski | Website by Dušan
-					Vuković{" "}
-				</p>
-			</Wrapper>
-		</footer>
-	)
-}
+  let year = new Date().getFullYear();
+  return (
+    <footer className={styles.footer}>
+      <Wrapper>
+        <p className={styles.footerCenter}>
+          {" "}
+          Copyright © {year} Milica Janevski | Website by{" "}
+          <a
+            className="white-color"
+            href="https://www.dusanvukovic.com/"
+            rel="noopener"
+            target="__blank"
+          >
+            Dušan Vuković
+          </a>{" "}
+        </p>
+      </Wrapper>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
